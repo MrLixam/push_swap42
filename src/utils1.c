1c5be29a71125d352c54aa6d72b4141c04f25e9f
MrLixam <103458566+MrLixam@users.noreply.github.com>
2023-06-05 12:37:13 +0200

    Update and rename utils1.c to src/utils1.c

--- src/utils1.c
+++ src/utils1.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../push_swap.h"
 
 void	ft_sx(t_list **stack, int x)
 {
