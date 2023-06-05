1a43152d05f823c5ee6f6872f7a294a5fb24f0e1
MrLixam <103458566+MrLixam@users.noreply.github.com>
2023-06-05 12:35:33 +0200

    Update and rename complex_sort.c to src/complex_sort.c

--- src/complex_sort.c
+++ src/complex_sort.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../push_swap.h"
 
 static void	send_back(t_list **sa, t_list **sb)
 {
