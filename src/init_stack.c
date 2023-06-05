6c8a4229715ad9bcb097be3f84ac12c230cce372
MrLixam <103458566+MrLixam@users.noreply.github.com>
2023-06-05 12:35:55 +0200

    Update and rename init_stack.c to src/init_stack.c

--- src/init_stack.c
+++ src/init_stack.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../push_swap.h"
 
 void	delete_stacks(t_list **stack_a, t_list **stack_b)
 {
