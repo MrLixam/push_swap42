ee4f0a0d9e88abb151e10a955576db6cd419ab78
MrLixam <103458566+MrLixam@users.noreply.github.com>
2023-06-05 12:37:34 +0200

    Update sort_main.c

--- src/sort_main.c
+++ src/sort_main.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../push_swap.h"
 
 static void	sort_3(t_list **stack, int s)
 {
